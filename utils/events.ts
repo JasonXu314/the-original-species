85aa1678d13ece24e064fe03036f9f3510dad2b1
Jason Xu <Xinwen31415@gmail.com>
2020-08-03 13:23:33 -0500

    Updated deadline

--- utils/events.ts
+++ utils/events.ts
@@ -5,7 +5,7 @@ export default [
 			'Hello and welcome to our first event! The Biology Bowl will be a 32 round, double elimination, Quiz Bowl-styled competition modeled after the National Science Bowl. The competition will only test Biology topics and will range in difficulty from AP-level to USABO-level based off of round progression. The competition will take place through Zoom on August 8th 2020.',
 		prizes: ['The Winning Team will receive 50 USD'],
 		registration: [
-			'Registration should be complete by the end of the day August 2nd.',
+			'Registration should be complete by the end of the day August 5th.',
 			'Only one member of each team should fill out a registration form.',
 			'Each team should fill out the registration form only once, if errors were made or updates are needed, please contact the TOS team.',
 			'After registration, you will receive an email confirming your registration within 2 days.'
