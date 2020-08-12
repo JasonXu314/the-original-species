bc57cbeebc931006b14aa34e95514d99b01dca8c
Jason Xu <Xinwen31415@gmail.com>
2020-08-12 05:22:03 -0500

    Added links for biology bowl

--- pages/events/biology-bowl.tsx
+++ pages/events/biology-bowl.tsx
@@ -26,6 +26,57 @@ const EventsEvent: NextPage = () => {
 					<img className={styles.bracket} src="/bio-bowl-bracket.svg" alt="Final Bracket" title="Final Bracket" />
 				</a>
 			</Link>
+			<h2>Important Documents</h2>
+			<ul className={styles.lists}>
+				<li>
+					<h3>Rounds</h3>
+					<ul className={styles.lists}>
+						<li>
+							<a href="https://docs.google.com/document/d/1Z3DFgnVHpa7Wbn3bqXTifoFd0ultBKjhDQgxXwvglTI/edit?usp=sharing">Round 1</a>
+						</li>
+						<li>
+							<a href="https://docs.google.com/document/d/1kOTktrc2VWsg3loolwt8tNh_ANmjjRDaogF3LU88Esg/edit?usp=sharing">Round 2</a>
+						</li>
+						<li>
+							<a href="https://docs.google.com/document/d/1R1vYDHYuD_T78x8fdannZ73A3vwMpY5NDsBihG1AFGo/edit?usp=sharing">Round 3</a>
+						</li>
+						<li>
+							<a href="https://docs.google.com/document/d/1HwZyBLBvVsPovEroQ4NujNBfIWbx-RJJYiYJkGIvgl0/edit?usp=sharing">Round 4</a>
+						</li>
+						<li>
+							<a href="https://docs.google.com/document/d/1gVlUecoxuborVnnhc7RIwl5vm2LKFoea8L7Q_gnrJZs/edit?usp=sharing">Round 5</a>
+						</li>
+						<li>
+							<a href="https://docs.google.com/document/d/1HmbTorafY9MvV6rwycfVKsNSPJLcobauVPJF6Hb9K3M/edit?usp=sharing">Round 6</a>
+						</li>
+						<li>
+							<a href="https://docs.google.com/document/d/1qPsmZzhY6A9Ax9LqX0ShO2FrPuVKSb9IQzKzATmsItc/edit?usp=sharing">Spares</a>
+						</li>
+					</ul>
+					<h4>Note: Questions have been edited after the competition to ensure accuracy and relevance</h4>
+				</li>
+				<li>
+					<h3>Visual Bonus Slides</h3>
+					<ul className={styles.lists}>
+						<li>
+							<a href="https://docs.google.com/presentation/d/1-m0qVcGg_b7r-V4hG-zeE058-yQef2Og7oZqmnOce38/edit?usp=sharing">Round 5</a>
+						</li>
+						<li>
+							<a href="https://docs.google.com/presentation/d/1st6T_CKkIjcqbX8Vfr4uQ7mV2RfBWnFKPux5Jo7Go2U/edit?usp=sharing">Round 6</a>
+						</li>
+						<li>
+							<a href="https://docs.google.com/presentation/d/1gTpJ-oJAcwF4gNw54b1SbUyg5AYyI_N6i3iGNLuH_o4/edit?usp=sharing">Round 7</a>
+						</li>
+					</ul>
+				</li>
+				<li>
+					<h3>Pretest</h3>
+					<a href="https://docs.google.com/document/d/1896S44RL528zHMSK8nyGIHWGF8_XCdPeHMuwWCEH69k/edit?usp=sharing">Pretest</a>
+					<br />
+					<a href="https://docs.google.com/document/d/1JMcZG_kMtjar2GFIOxTl26RY9Ay0pSsXYTpPikKIoEE/edit?usp=sharing">Pretest - Key</a>
+					<h4>This test was used before the competition to assess the abilities of each team and create the seed</h4>
+				</li>
+			</ul>
 			<h1 className={styles.alert}>Note: This event has concluded!</h1>
 			<br />
 			<h1>Details for Biology Bowl</h1>
